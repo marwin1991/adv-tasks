fba43517ad8463c9193230aee9b603442646d9b8
Peter <peter.zmilczak@gmail.com>
2022-07-03 12:00:53 +0200

    user model implementation

---
+++ task1-spring-boot-react-google-oauth2/backend/src/main/java/com/codecool/marwin1991/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.codecool.marwin1991.controller;
+
+import com.codecool.marwin1991.exception.ResourceNotFoundException;
+import com.codecool.marwin1991.model.AppUser;
+import com.codecool.marwin1991.repository.UserRepository;
+import com.codecool.marwin1991.security.CurrentUser;
+import com.codecool.marwin1991.security.UserPrincipal;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/profile")
+    @PreAuthorize("hasRole('USER')")
+    public AppUser getCurrentUser(@CurrentUser UserPrincipal userPrincipal){
+        //UserPrincipal userPrincipal1 = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+}
